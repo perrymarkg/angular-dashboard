e274355d205742492612dda2c1c47006dfa0fada
Perry <perry.mark@zeald.com>
2018-03-02 21:39:22 +0800

    pagination service

---
+++ src/app/services/pagination.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class PaginationService {
+
+  data = {
+    items: false,
+    totalPages:[],
+    rangeStart:0,
+    rangeEnd:0,
+    currentPage:1
+  }
+  constructor() {}
+
+  paginate(items, currentPage = 1, perPage = 5){
+    
+    if( items.length ){
+      const total = Math.round(items.length / perPage)
+      this.data.currentPage = currentPage;
+      this.data.totalPages = Array(total).fill(0).map((x,i)=>i) ;
+      this.data.rangeStart = ( (this.data.currentPage * perPage ) - perPage);
+      this.data.rangeEnd = (this.data.currentPage * perPage) - 1
+      
+      return this.data;
+      
+    }
+    
+  }
+
+
+}
