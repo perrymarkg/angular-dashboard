ad4286e79933aba3db19169134a9d1023392340b
Perry <perry.mark@zeald.com>
2018-03-04 14:27:19 +0800

    env

---
+++ src/app/env/env-tpl.ts
@@ -0,0 +1,11 @@
+export const env = {
+  production: false,
+  firebase: {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
+  }
+};
